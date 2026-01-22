d4198eb9c712d0e470555c110485469f56ab97cc
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-01-22 08:00:11 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ls;
-        map<int,int> mpp;
-        int n = nums.size();
-        int m = (int)(n/3)+1;
-        for(int i = 0; i< n; i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==m){
-                ls.push_back(nums[i]);
+        int ct1 = 0, ct2 = 0;
+        long el1 = LONG_MIN;
+        long el2 = LONG_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(ct1==0 && el1!=nums[i]){
+                ct1 = 1;
+                el1 = nums[i];
             }
-            if(ls.size()==2)break;
+            else if(ct2==0 && el2!=nums[i]){
+                ct2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i]==el1)ct1++;
+            else if(nums[i]==el2)ct2++;
+            else{
+                ct1--;
+                ct2--;
+            }
         }
+        vector<int> ls;
+        ct1 = 0,ct2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(el1 == nums[i]) ct1++;
+            else if(el2 == nums[i]) ct2++;
+        }
+        int m = (int)(nums.size()/3) + 1;
+        if(ct1 >= m) ls.push_back(el1);
+        if(ct2 >= m) ls.push_back(el2);
         sort(ls.begin(),ls.end());
         return ls;
     }
