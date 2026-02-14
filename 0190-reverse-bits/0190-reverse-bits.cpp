201d7383cbd4ec420d7e4352360e3d72ac22c2a1
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-02-14 10:04:15 -0800

    Time: 2 ms (37.43%), Space: 8.3 MB (46.04%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i=1;i<=32;i++){
+            res = res + (n&1);
+            n = n >> 1;
+            if(i<=31)
+               res = res << 1;
+        }
+        return res;
+    }
+};
