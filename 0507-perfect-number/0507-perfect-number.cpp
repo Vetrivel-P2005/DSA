4d4941a04c50bea1f6d394caaea478011e532bbc
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-03-12 23:08:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,12 +3,15 @@ public:
     bool checkPerfectNumber(int num) {
         if(num <= 1)
             return false;
-        int s = 0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0){
-                s+=i;
+        int sum = 1;
+         for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) { 
+                    sum += num / i;
+                }
             }
         }
-        return s == num;
+        return sum == num;
     }
 };
