b62022cf453161f9ed9cd230f7fd155e83fbc280
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-04-16 23:38:58 +0530

    Time: 0 ms (100%), Space: 9.1 MB (22.88%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int opened = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                if (opened > 0) result += c;
+                opened++;
+            } else {
+                opened--;
+                if (opened > 0) result += c;
+            }
+        }
+        
+        return result;
+    }
+};
