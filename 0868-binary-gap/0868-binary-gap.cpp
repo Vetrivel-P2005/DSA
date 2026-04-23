cad4c7dcd9989767915a89a6acb0aecdf15c0ff8
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-04-23 20:53:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -3,7 +3,6 @@ public:
     int binaryGap(int n) {
         int max_gap = 0;
         int last_pos = -1;
-        
         for (int i = 0; i < 31; ++i) {
             if ((n >> i) & 1) {
                 if (last_pos != -1) {
