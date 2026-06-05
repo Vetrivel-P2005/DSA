0284300e27d3c5454d183998c844c59757662859
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-06-05 21:27:14 +0530

    Time: 0 ms (100%), Space: 14 MB (60.78%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+        
+        sort(candidates.begin(), candidates.end());
+        
+        backtrack(candidates, target, 0, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(const vector<int>& candidates, int target, int startIndex, 
+                   vector<int>& current, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.size(); ++i) {
+            if (candidates[i] > target) {
+                break;
+            }
+
+            if (i > startIndex && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            current.push_back(candidates[i]);
+            
+            backtrack(candidates, target - candidates[i], i + 1, current, result);
+            
+            current.pop_back();
+        }
+    }
+};
