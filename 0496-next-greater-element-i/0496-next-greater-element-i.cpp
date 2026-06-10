995eb444369e2ff0b185a2029059b47033441b1f
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-06-10 22:06:00 +0530

    Time: 0 ms (100%), Space: 12.6 MB (75.26%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       std::unordered_map<int, int> next_greater;
+        std::stack<int> s;
+
+        for (int num : nums2) {
+            while (!s.empty() && s.top() < num) {
+                next_greater[s.top()] = num;
+                s.pop();
+            }
+            s.push(num);
+        }
+
+        vector<int> ans;
+        ans.reserve(nums1.size());
+        for (int num : nums1) {
+            if (next_greater.count(num)) {
+                ans.push_back(next_greater[num]);
+            } else {
+                ans.push_back(-1);
+            }
+        }
+
+        return ans; 
+    }
+};
