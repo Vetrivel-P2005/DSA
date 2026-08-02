8f227cfedcf4f14a7da9a66ebce46857417acd0d
Vetrivel-P2005 <vetrivel13022005@gmail.com>
2026-08-02 13:39:59 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<int> dp(n);
-        for (int i = 0; i < n; ++i) {
-            dp[i] = piles[i];
-        }
-
-        for (int diff = 1; diff < n; ++diff) {
-            for (int i = 0; i < n - diff; ++i) {
-                int j = i + diff;
-                dp[i] = max(piles[i] - dp[i + 1], piles[j] - dp[i]);
-            }
-        }
-        return dp[0] >= 0;
+      return  true;
     }
 };
